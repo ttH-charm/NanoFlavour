e706ecf7f22210644d8c94858b1a22862040f57c
Huilin Qu <huilin.qu@cern.ch>
2023-10-25 18:25:33 +0200

    Fix stat unc in flavor tagging SFs.
     - `20231005_ak4puppi_OS__fit_individual__floatVplusHF__fix_FreezeAll_for_StatUnc`

--- python/producers/flavTagSFProducer.py
+++ python/producers/flavTagSFProducer.py
@@ -7,29 +7,6 @@ ROOT.PyConfig.IgnoreCommandLineOptions = True
 from PhysicsTools.NanoAODTools.postprocessing.framework.eventloop import Module
 
 
-era_dict = {2015: '2016preVFP_UL', 2016: '2016postVFP_UL', 2017: '2017_UL', 2018: '2018_UL'}
-# FIXME: systematics list
-systematics = [
-    'Stat',
-    'LHEScaleWeight_muF_ttbar',
-    'LHEScaleWeight_muF_wjets',
-    'LHEScaleWeight_muF_zjets',
-    'LHEScaleWeight_muR_ttbar',
-    'LHEScaleWeight_muR_wjets',
-    'LHEScaleWeight_muR_zjets',
-    'PSWeightISR',
-    'PSWeightFSR',
-    'XSec_WJets_c',
-    'XSec_WJets_b',
-    'XSec_ZJets_c',
-    'XSec_ZJets_b',
-    'JER',
-    'JES',
-    'PUWeight',
-    # 'PUJetID'
-]
-
-
 # def debug(msg, activated=True):
 #     if activated:
 #         print(' > ', msg)
@@ -44,27 +21,60 @@ def rndSeed(event, jets, extra=0):
 
 class FlavTagSFProducer(Module):
 
-    def __init__(self, year, weight_name='flavTagWeight'):
+    def __init__(self, year, weight_name='flavTagWeight', split_stat_unc=True):
         era = {2015: '2016preVFP_UL', 2016: '2016postVFP_UL', 2017: '2017_UL', 2018: '2018_UL'}[year]
         correction_file = os.path.expandvars(
             f'$CMSSW_BASE/src/PhysicsTools/NanoFlavour/data/flavTagSF/flavTaggingSF_{era}.json.gz')
         self.corr = correctionlib.CorrectionSet.from_file(correction_file)['particleNetAK4_shape']
         self.name = weight_name
 
+        self.systematics = [
+            'Stat',
+            'LHEScaleWeight_muF_ttbar',
+            'LHEScaleWeight_muF_wjets',
+            'LHEScaleWeight_muF_zjets',
+            'LHEScaleWeight_muR_ttbar',
+            'LHEScaleWeight_muR_wjets',
+            'LHEScaleWeight_muR_zjets',
+            'PSWeightISR',
+            'PSWeightFSR',
+            'XSec_WJets_c',
+            'XSec_WJets_b',
+            'XSec_ZJets_c',
+            'XSec_ZJets_b',
+            'JER',
+            'JES',
+            'PUWeight',
+            # 'PUJetID'
+        ]
+        if split_stat_unc:
+            flavors = ['flavB', 'flavC', 'flavL']
+            tag_categories = ['C0', 'C1', 'C2', 'C3', 'C4', 'B0', 'B1', 'B2', 'B3', 'B4']
+            kin_categories = [
+                'eta_0p00toinf_pt_25to40',
+                'eta_0p00toinf_pt_40to60',
+                'eta_0p00toinf_pt_60to100',
+                'eta_0p00toinf_pt_100toinf',
+            ]
+            for flav in flavors:
+                for tag in tag_categories:
+                    for kin in kin_categories:
+                        self.systematics.append(f'Stat_{flav}_{tag}_{kin}')
+
     def beginFile(self, inputFile, outputFile, inputTree, wrappedOutputTree):
         self.isMC = bool(inputTree.GetBranch('genWeight'))
         if self.isMC:
             self.out = wrappedOutputTree
             self.basewgts = {self.name: 1.}
-            for syst in systematics:
+            for syst in self.systematics:
                 self.basewgts[f'{self.name}_{syst}_UP'] = 1.
                 self.basewgts[f'{self.name}_{syst}_DOWN'] = 1.
             for name in self.basewgts.keys():
                 self.out.branch(name, "F")
 
-            # ====== testing ======
-            for name in self.basewgts.keys():
-                self.out.branch('genMatched_' + name, "F")
+            # # ====== testing ======
+            # for name in self.basewgts.keys():
+            #     self.out.branch('genMatched_' + name, "F")
 
     def compute_weights(self, event, jets, n_toys=1000):
         n = len(jets)
@@ -98,7 +108,7 @@ class FlavTagSFProducer(Module):
         wgts[f'{self.name}_{syst}_DOWN'] = wgt_stat_dn
 
         # systematics
-        for syst in systematics:
+        for syst in self.systematics:
             if syst == 'Stat':
                 continue
             wgts[f'{self.name}_{syst}_UP'] = self.corr.evaluate(f'up_{syst}', hflav, wp, abseta, pt).prod()
@@ -120,11 +130,11 @@ class FlavTagSFProducer(Module):
         for name, val in wgts.items():
             self.out.fillBranch(name, np.clip(val, 0.3, 3))
 
-        # ====== testing ======
-        gen_matched_jets = [j for j in event.ak4jets if j.genJetIdx >= 0]
-        wgts_genMatched = self.compute_weights(event, gen_matched_jets)
-        for name, val in wgts_genMatched.items():
-            self.out.fillBranch('genMatched_' + name, np.clip(val, 0.3, 3))
+        # # ====== testing ======
+        # gen_matched_jets = [j for j in event.ak4jets if j.genJetIdx >= 0]
+        # wgts_genMatched = self.compute_weights(event, gen_matched_jets)
+        # for name, val in wgts_genMatched.items():
+        #     self.out.fillBranch('genMatched_' + name, np.clip(val, 0.3, 3))
 
         return True
 
